71cb1df4d28001a4af9e1d3b9d87aef013c2150b
Pokfinner <finkry@gmail.com>
2023-11-20 08:29:43 -0300

    inicializar servidor

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Servidor do Diogo!');
+});
+
+app.get('/todos', (req, res) => {
+
+    const todos = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+        }
+    ]
+
+    res.json(todos)
+
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
